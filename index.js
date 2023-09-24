1c7de1287dc44150e562699adbb677a248d3d64f
BT08249 <adarshgku@gmail.com>
2023-09-24 14:58:18 +0530

    added condition for checking if the song artist exists

--- index.js
+++ index.js
@@ -114,37 +114,174 @@ const startSong = async () => {
 
 const randomSong = async (genre) => {
   const genreSelected = genre;
-  console.log(genreSelected)
+  console.log(genreSelected);
 
-  if(genreSelected === "random") {
-    const genre = ["acoustic", "afrobeat", "alt-rock", "alternative", "ambient", "anime", "black-metal", "bluegrass", "blues", "bossanova", "brazil", "breakbeat", "british", "cantopop", "chicago-house", "children", "chill", "classical", "club", "comedy", "country", "dance", "dancehall", "death-metal", "deep-house", "detroit-techno", "disco", "disney", "drum-and-bass", "dub", "dubstep", "edm", "electro", "electronic", "emo", "folk", "forro", "french", "funk", "garage", "german", "gospel", "goth", "grindcore", "groove", "grunge", "guitar", "happy", "hard-rock", "hardcore", "hardstyle", "heavy-metal", "hip-hop", "holidays", "honky-tonk", "house", "idm", "indian", "indie", "indie-pop", "industrial", "iranian", "j-dance", "j-idol", "j-pop", "j-rock", "jazz", "k-pop", "kids", "latin", "latino", "malay", "mandopop", "metal", "metal-misc", "metalcore", "minimal-techno", "movies", "mpb", "new-age", "new-release", "opera", "pagode", "party", "philippines-opm", "piano", "pop", "pop-film", "post-dubstep", "power-pop", "progressive-house", "psych-rock", "punk", "punk-rock", "r-n-b", "rainy-day", "reggae", "reggaeton", "road-trip", "rock", "rock-n-roll", "rockabilly", "romance", "sad", "salsa", "samba", "sertanejo", "show-tunes", "singer-songwriter", "ska", "sleep", "songwriter", "soul", "soundtracks", "spanish", "study", "summer", "swedish", "synth-pop", "tango", "techno", "trance", "trip-hop", "turkish", "work-out", "world-music"]
+  if (genreSelected === "random") {
+    const genre = [
+      "acoustic",
+      "afrobeat",
+      "alt-rock",
+      "alternative",
+      "ambient",
+      "anime",
+      "black-metal",
+      "bluegrass",
+      "blues",
+      "bossanova",
+      "brazil",
+      "breakbeat",
+      "british",
+      "cantopop",
+      "chicago-house",
+      "children",
+      "chill",
+      "classical",
+      "club",
+      "comedy",
+      "country",
+      "dance",
+      "dancehall",
+      "death-metal",
+      "deep-house",
+      "detroit-techno",
+      "disco",
+      "disney",
+      "drum-and-bass",
+      "dub",
+      "dubstep",
+      "edm",
+      "electro",
+      "electronic",
+      "emo",
+      "folk",
+      "forro",
+      "french",
+      "funk",
+      "garage",
+      "german",
+      "gospel",
+      "goth",
+      "grindcore",
+      "groove",
+      "grunge",
+      "guitar",
+      "happy",
+      "hard-rock",
+      "hardcore",
+      "hardstyle",
+      "heavy-metal",
+      "hip-hop",
+      "holidays",
+      "honky-tonk",
+      "house",
+      "idm",
+      "indian",
+      "indie",
+      "indie-pop",
+      "industrial",
+      "iranian",
+      "j-dance",
+      "j-idol",
+      "j-pop",
+      "j-rock",
+      "jazz",
+      "k-pop",
+      "kids",
+      "latin",
+      "latino",
+      "malay",
+      "mandopop",
+      "metal",
+      "metal-misc",
+      "metalcore",
+      "minimal-techno",
+      "movies",
+      "mpb",
+      "new-age",
+      "new-release",
+      "opera",
+      "pagode",
+      "party",
+      "philippines-opm",
+      "piano",
+      "pop",
+      "pop-film",
+      "post-dubstep",
+      "power-pop",
+      "progressive-house",
+      "psych-rock",
+      "punk",
+      "punk-rock",
+      "r-n-b",
+      "rainy-day",
+      "reggae",
+      "reggaeton",
+      "road-trip",
+      "rock",
+      "rock-n-roll",
+      "rockabilly",
+      "romance",
+      "sad",
+      "salsa",
+      "samba",
+      "sertanejo",
+      "show-tunes",
+      "singer-songwriter",
+      "ska",
+      "sleep",
+      "songwriter",
+      "soul",
+      "soundtracks",
+      "spanish",
+      "study",
+      "summer",
+      "swedish",
+      "synth-pop",
+      "tango",
+      "techno",
+      "trance",
+      "trip-hop",
+      "turkish",
+      "work-out",
+      "world-music",
+    ];
     const randomGenre = Math.floor(Math.random() * genre.length);
     genreSelected = genre[randomGenre];
   }
 
-  const response = await fetch(`https://api.spotify.com/v1/recommendations?seed_genres=${genreSelected}&limit=1`, {
-    method: "GET",
-    headers: { Authorization: `Bearer ${accessToken}` },
-  })
+  const response = await fetch(
+    `https://api.spotify.com/v1/recommendations?seed_genres=${genreSelected}&limit=1`,
+    {
+      method: "GET",
+      headers: { Authorization: `Bearer ${accessToken}` },
+    }
+  );
 
   const data = await response.json();
 
   const artists = [];
 
-  data.tracks[0].artists.forEach((artist) => {
-    artists.push({ name: artist.name });
-    
-  });
+  if (data.tracks[0].artists) {
+    data.tracks[0].artists.forEach((artist) => {
+      artists.push({ name: artist.name });
+    });
+  }
 
-  const responseArtist = await fetch("https://api.spotify.com/v1/me/player/play", {
-    method: "PUT",
-    headers: { Authorization: `Bearer ${accessToken}`, "Content-Type": "application/json" },
-    body: JSON.stringify({
-      uris: [data.tracks[0].uri]
-    })
-  });
+  const responseArtist = await fetch(
+    "https://api.spotify.com/v1/me/player/play",
+    {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        uris: [data.tracks[0].uri],
+      }),
+    }
+  );
 
-  return { name: data.tracks[0].name, artists};
-}
+  return { name: data.tracks[0].name, artists };
+};
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
